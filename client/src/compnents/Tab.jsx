f7745ce86bfe99fd7758e8ee51bdd9eb77ac3991
Zyan Baishya <zaanbaishya00@gmail.com>
2023-09-03 04:44:36 -0700

    home page fixed

--- client/src/compnents/Tab.jsx
+++ client/src/compnents/Tab.jsx
@@ -4,23 +4,18 @@ import { useSnapshot } from 'valtio'
 import state from '../store';
 
 const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
-    const snap = useSnapshot(state);
+  const snap = useSnapshot(state);
 
-    const activeStyles = isFilterTab && isActiveTab?
-    {
-        backgroundColor : snap.color,
-        opacity : 0.5
-    }:{
-        backgroundColor : "transparent",
-        opacity : 1
-    }
+  const activeStyles = isFilterTab && isActiveTab 
+    ? { backgroundColor: snap.color, opacity: 0.5 }
+    : { backgroundColor: "transparent", opacity: 1 }
 
-    return (
+  return (
     <div
-        key={tab.name}
-        className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4'}`}
-        onClick={handleClick}
-        style={activeStyles}
+      key={tab.name}
+      className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' : 'rounded-4'}`}
+      onClick={handleClick}
+      style={activeStyles}
     >
       <img 
         src={tab.icon}
